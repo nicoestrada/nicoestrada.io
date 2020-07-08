2686fa52385e6619cdd5a5323452bab4ba7ff1da
Nico <59927633+nicoestrada@users.noreply.github.com>
2020-07-08 13:28:19 -0500

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -24,7 +24,7 @@ module.exports = {
         },
         {
             name: "Github",
-            url: "https://github.com/estradanicolas"
+            url: "https://github.com/nicoestrada"
         },
     ],
     
